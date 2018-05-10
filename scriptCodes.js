c260e408a8d7db4a7bdb462cef8e428434d34656
Marie Stephanie Alesna <istephielicious@gmail.com>
2018-05-10 23:45:37 +0200

    New Adjustments

--- scriptCodes.js
+++ scriptCodes.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
+/*Setup date */
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1;
+	var yyyy = today.getFullYear();
+	if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+	today = yyyy+'-'+mm+'-'+dd;
+	$('#sched').attr("min", today);
+/*Setup sidebar*/
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+        return;
     });
-
 });
 
 function hideSideNav() {
 	$('#sidebar').toggleClass('active', true);
 
 } 
+
+function showSideNav() {
+	$('#sidebar').toggleClass('active', false);
+}
