9b7bde5597ae87099fe4214ccf64e0d750dd279a
Marie Stephanie Alesna <istephielicious@gmail.com>
2018-05-14 17:26:50 +0200

    needs editing

--- scriptCodes.js
+++ scriptCodes.js
@@ -1,29 +1,16 @@
 $(document).ready(function () {
-/*Setup date */
-	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1;
-	var yyyy = today.getFullYear();
-	if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
-	today = yyyy+'-'+mm+'-'+dd;
-	$('#sched').attr("min", today);
-/*Setup sidebar*/
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        return;
-    });
+
+/**HOME */
+document.getElementById("indexP").innerHTML = 
+    "This is a sample webpage to practice manipulating the DOM(Document Object Model). [click me] ";
+document.getElementById("indexP").addEventListener("click",function(){alert("Hello there!, to start with, go to About tab")},false);
+
+
+/**ABOUT */
+document.getElementById("numFlr").addEventListener("change", setupFlrPlan(),false);
 });
 
-function hideSideNav() {
-	$('#sidebar').toggleClass('active', true);
-
-} 
-
-function showSideNav() {
-	$('#sidebar').toggleClass('active', false);
+function setupFlrPlan() {
+    
 }
+
